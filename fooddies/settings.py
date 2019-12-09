0e6624983b38d5a5211ebea53c353f7955b442a9
Justine Quiapos <jmzquiapos@outlook.com>
2019-12-09 16:49:44 +0000

    SO CLOSE

--- fooddies/settings.py
+++ fooddies/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '*r!%z3!4tng0wz8p+rm)rnb)d-sb$)j6oh14^nr(f!bel213(!'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['fooddies.space','localhost']
 
 
 # Application definition
@@ -165,7 +165,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATEFILES_DIR = [
+        os.path.join(BASE_DIR, 'build/static')
+        ]
 
+STATIC_ROUTE = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'build/static/')
 
 # Extra Settings
 
